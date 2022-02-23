a65efd307ff8d11f9303e1d7a68a72a7a4575f00
joaovitordantas <joaovitordantasss@hotmail.com>
2022-02-23 13:12:38 -0300

    Insert conditional rendering on app.js

--- pomodoro-app/src/App.js
+++ pomodoro-app/src/App.js
@@ -1,12 +1,21 @@
+import { useContext } from "react";
 import SetPomodoro from "./components/SetPomodoro";
+import { SettingContext } from "./context/settingsContext";
 
 
 function App() {
+  const {pomodoro} = useContext(SettingContext);
   return (
     <div className="container">
      <h1>Pomodoro</h1>
      <small>Be productive the right way.</small>
-     <SetPomodoro />
+      {pomodoro === 0 ? 
+        <SetPomodoro /> : 
+        <>
+        <h3>Testing if set is 0</h3>
+        </>
+      }
+     
 
     </div>
   );
